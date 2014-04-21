ff3b334e12652e5641810c594f83f8bc504fae23
Geoff Lawler <glawler@tislabs.com>
2014-04-21 13:34:32 -0400

    moved generic property handling into base class for BGG Python objects. No functional change, just restructuring. should be more simple to add the remaining BGG objects now.

--- libBGG/Boardgame.py
+++ libBGG/Boardgame.py
@@ -1,15 +1,19 @@
 import logging
 
+from libBGG.PropertiedObject import PropertiedObject
+
 log = logging.getLogger(__name__)
+
 
 class BoardgameException(Exception):
     '''Exception wrapper for Boardgame specific exceptions.'''
     pass
 
 
-class Boardgame(object):
+class Boardgame(PropertiedObject):
     '''Store information about a boardgame. The init function takes a list of valid
-    proprties defined by boardgame.valid_properties.'''
+    proprties defined by boardgame.valid_properties. Properties that are lists can
+    be given as a single item or as a list.'''
 
     # This should really contain the correct types as well...
     valid_properties = [
@@ -20,27 +24,14 @@ class Boardgame(object):
     ]
 
     def __init__(self, **kwargs):
-        # set all attributes to None
-        for vp in Boardgame.valid_properties:
-            setattr(self, '_' + vp, '')
-
-        # if we're given a valid property, set it otherwise leave it
-        # as None
-        for key, value in kwargs.iteritems():
-            if key in Boardgame.valid_properties:
-                setattr(self, '_' + key, value)    
-            else:
-                err = 'bad arg given to Boardgame: %s=%s' % (
-                    str(key), str(value))
-                log.critical(err)
-                raise BoardgameException(err)
-
+        self.valid_properties = Boardgame.valid_properties
+        super(Boardgame, self).__init__(**kwargs)
         # force lists to be lists
         for l in ['designers', 'artists', 'categories', 'mechanics', 'families', 'publishers',
                   'names']:
             try:
-                if type(getattr(self, '_' + l)) != list:
-                    setattr(self, '_' + l, [getattr(self, '_' + l)])
+                if type(getattr(self, l)) != list:
+                    setattr(self, l, [getattr(self, l)])
             except AttributeError:
                 pass
 
@@ -51,116 +42,13 @@ class Boardgame(object):
         return self.__unicode__().encode('utf-8').strip()
 
     def dump(self):
-        print '%s:' % self.name
-        for p in sorted(Boardgame.valid_properties):
-            try:
-                attr = getattr(self, '_' + p)
-                if len(attr) > 0:
-                    if type(attr) == list:
-                        print '\t%s: %s' % (p, u', '.join(attr).encode('utf-8').strip())
-                    else:
-                        print '\t%s: %s' % (p, attr.encode('utf-8').strip())
-            except AttributeError:
-                pass
+        super(Boardgame, self).dump('%s' % self.name)
 
+    # Litte syntactic sugar for the more usual case of a single name.
     @property
     def name(self):
-        return self._names[0]
+        return self.names[0]
 
     @name.setter
     def name(self, value):
-        self._names = [value]
-
-    @property
-    def names(self):
-        return self._names
-
-    @names.setter
-    def names(self, value):
-        if type(value) == list:
-            log.debug('setting names from list')
-            self._names = value
-        else:
-            log.debug('setting names from string')
-            self._names = [value]
-
-    @property
-    def thumbnail(self):
-        return self._thumbnail
-
-    @thumbnail.setter
-    def thumbnail(self, value):
-        self._thumbnail = value
-
-    @property
-    def image(self):
-        return self._image
-
-    @image.setter
-    def image(self, value):
-        self._image = value
-
-    @property
-    def description(self):
-        return self._description
-
-    @description.setter
-    def description(self, value):
-        self._description = value
-
-    @property
-    def year(self):
-        return self._year
-
-    @year.setter
-    def year(self, value):
-        self._year = year
-
-    @property
-    def minplayers(self):
-        return self._minplayers
-
-    @minplayers.setter
-    def minplayers(self, value):
-        self._minplayers = value
-
-    @property
-    def maxplayers(self):
-        return self._maxplayers
-
-    @maxplayers.setter
-    def maxplayers(self, value):
-        self._maxplayers = value
-
-    @property
-    def categories(self):
-        return self._categories
-
-    @categories.setter
-    def categories(self, value):
-        self._categories = value
-
-    @property
-    def mechanics(self):
-        return self._mechanics
-
-    @mechanics.setter
-    def mechanics(self, value):
-        self._mechanics = value
-
-    @property
-    def families(self):
-        return self._families
-
-    @families.setter
-    def families(self, value):
-        self._families = value
-
-    @property
-    def publishers(self):
-        return self._publishers
-
-    @publishers.setter
-    def publishers(self, value):
-        self._publishers = value
-
+        self.names = [value]
--- libBGG/Guild.py
+++ libBGG/Guild.py
@@ -1,5 +1,5 @@
-
 import logging
+from libBGG.PropertiedObject import PropertiedObject
 
 log = logging.getLogger(__name__)
 
@@ -8,7 +8,7 @@ class GuildException(Exception):
     pass
 
 
-class Guild(object):
+class Guild(PropertiedObject):
     '''Store information about a BGG Guild. The init function takes a list of valid
     proprties defined by Guild.valid_properties.'''
 
@@ -19,96 +19,21 @@ class Guild(object):
     ]
 
     def __init__(self, **kwargs):
-        for vp in Guild.valid_properties:
-            setattr(self, '_' + vp, '')
+        self.valid_properties = Guild.valid_properties
+        super(Guild, self).__init__(**kwargs)
 
-        # if we're given a valid property, set it otherwise leave it
-        # as None
-        for key, value in kwargs.iteritems():
-            if key in Guild.valid_properties:
-                setattr(self, '_' + key, value)    
-            else:
-                err = 'bad arg given to Boardgame: %s=%s' % (
-                    str(key), str(value))
-                log.critical(err)
-                raise GuildException(err)
+        # force self.members to be a list.
         try:
             if type(self._members) != list:
                 self._members = [self._members]
         except AttributeError:
             pass
 
+    def dump(self):
+        super(Guild, self).dump('Guild %s' % self.name)
+
     def __unicode__(self):
         return 'Guild %s (id=%s): %s' % (self.name, self.bggid)
 
     def __str__(self):
         return self.__unicode__().encode('utf-8').strip()
-
-    def dump(self):
-        print 'Guild %s:' % self.name
-        for p in sorted(Guild.valid_properties):
-            try:
-                attr = getattr(self, '_' + p)
-                if attr != '':
-                    if type(attr) == list:
-                        print '\t%s: %s' % (p, u', '.join(attr).encode('utf-8').strip())
-                    else:
-                        print '\t%s: %s' % (p, attr.encode('utf-8').strip())
-            except AttributeError:
-                pass
-
-    @property
-    def name(self):
-        return self._name
-
-    @name.setter
-    def category(self, value):
-        self._name = value
-
-    @property
-    def bggid(self):
-        return self._bggid
-
-    @bggid.setter
-    def bggid(self, value):
-        self._bggid = value
-
-    @property
-    def category(self):
-        return self._category
-
-    @category.setter
-    def category(self, value):
-        self._category = value
-
-    @property
-    def website(self):
-        return self._website
-
-    @website.setter
-    def website(self, value):
-        self._website = value
-
-    @property
-    def manager(self):
-        return self._manager
-
-    @manager.setter
-    def manager(self, value):
-        self._manager = value
-
-    @property
-    def description(self):
-        return self._description
-
-    @description.setter
-    def description(self, value):
-        self._description = value
-
-    @property
-    def members(self):
-        return self._members
-
-    @members.setter
-    def members(self, value):
-        self._members = members
